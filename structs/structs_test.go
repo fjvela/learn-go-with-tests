3348ff043e51c356f9fc63bf56b3c61850a18d4e
Javier Vela <fjvela@gmail.com>
2025-04-16 15:30:11 +0200

    structs - add test perimeter
    Signed-off-by: Javier Vela <fjvela@gmail.com>

---
+++ structs/structs_test.go
@@ -0,0 +1,11 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 20.0)
+	want := 60.0
+	if got != want {
+		t.Errorf("got %g want %g", got, want)
+	}
+}
