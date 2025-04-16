4947fdbffb55feb42b883d653291a7d66ff6d3a3
Javier Vela <fjvela@gmail.com>
2025-04-16 15:01:06 +0200

    arrays - add test any size
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,11 +3,25 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		got := Sum(numbers)
+		want := 45
+
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+
 }
