9c57d71156373807086981226f6ed67e25357389
Javier Vela <fjvela@gmail.com>
2025-04-16 14:36:30 +0200

    integers - testeable examples
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +12,16 @@ func TestAdder(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected %d but got %d", expected, sum)
 	}
+}
+
+// Example functions are compiled whenever tests are executed.
+// Because such examples are validated by the Go compiler, you can be confident your documentation's
+//
+//	examples always reflect current code behavior.
+//
+// https://go.dev/blog/examples
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
