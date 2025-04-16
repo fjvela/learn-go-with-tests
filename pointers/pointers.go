ba9ef4efe0baf042988d3e37859985b211a2a347
Javier Vela <fjvela@gmail.com>
2025-04-16 18:01:33 +0200

    points - wallet fix tests
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,10 +1,13 @@
 package pointers
 
-type Wallet struct{}
+type Wallet struct {
+	balance float64
+}
 
 func (w *Wallet) Deposit(amount float64) {
+	w.balance += amount
 }
 
 func (w *Wallet) Balance() float64 {
-	return 0
+	return w.balance
 }
