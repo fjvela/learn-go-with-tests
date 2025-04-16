783c44ee08e340dd8bb659264fb85caa57f763ac
Javier Vela <fjvela@gmail.com>
2025-04-16 15:37:20 +0200

    structs - refactor use struct Rectangle
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -3,7 +3,9 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 20.0)
+	rectangle := Rectangle{10.0, 20.0}
+
+	got := Perimeter(rectangle)
 	want := 60.0
 	if got != want {
 		t.Errorf("got %g want %g", got, want)
@@ -11,7 +13,8 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(10.0, 20.0)
+	rectangle := Rectangle{10.0, 20.0}
+	got := Area(rectanble)
 	want := 200.0
 	if got != want {
 		t.Errorf("got %g want %g", got, want)
