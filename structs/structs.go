f33e75d35943923340b3c1abf376ead69be97536
Javier Vela <fjvela@gmail.com>
2025-04-16 15:47:19 +0200

    structs - area fix tests
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- structs/structs.go
+++ structs/structs.go
@@ -1,5 +1,7 @@
 package structs
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -18,8 +20,8 @@ func Area(rectangle Rectangle) float64 {
 }
 
 func (r Rectangle) Area() float64 {
-	return 0
+	return r.Height * r.Width
 }
 func (c Circle) Area() float64 {
-	return 0
+	return c.Radius * c.Radius * math.Pi
 }
