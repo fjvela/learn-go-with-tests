6ef132d39c12d3782eeac671ed923aad4fc75e89
Javier Vela <fjvela@gmail.com>
2025-04-16 18:05:03 +0200

    points - wallet add logs (diff between copy / ref)
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,13 +1,17 @@
 package pointers
 
+import "fmt"
+
 type Wallet struct {
 	balance float64
 }
 
 func (w *Wallet) Deposit(amount float64) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
 	w.balance += amount
 }
 
 func (w *Wallet) Balance() float64 {
+	// https://go.dev/ref/spec#Method_values
 	return w.balance
 }
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -1,6 +1,9 @@
 package pointers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWaller(t *testing.T) {
 	wallet := Wallet{}
@@ -9,6 +12,8 @@ func TestWaller(t *testing.T) {
 
 	got := wallet.Balance()
 	want := 10.0
+
+	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
 
 	if got != want {
 		t.Errorf("got %g want %g", got, want)
