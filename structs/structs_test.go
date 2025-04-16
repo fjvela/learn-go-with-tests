70a7fb32d4ff77a856dad03229923267e2f82111
Javier Vela <fjvela@gmail.com>
2025-04-16 15:43:31 +0200

    structs - add test area of circle
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -13,10 +13,22 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 20.0}
-	got := Area(rectangle)
-	want := 200.0
-	if got != want {
-		t.Errorf("got %g want %g", got, want)
-	}
+
+	t.Run("Area of rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 20.0}
+		got := Area(rectangle)
+		want := 200.0
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
+	t.Run("Area of circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := Area(circle)
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
