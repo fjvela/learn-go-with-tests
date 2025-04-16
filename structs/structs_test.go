d797497399e7368f2fb50689a8b3f5f515249b6b
Javier Vela <fjvela@gmail.com>
2025-04-16 15:53:49 +0200

    structs - refactor (to use interfaces - update test)
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,23 +14,19 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
 
-	t.Run("Area of rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 20.0}
-		want := 200.0
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("Area of circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
 }
