1bb22cadfe7902d8a4f4d639dc14726afd5bfd4a
Javier Vela <fjvela@gmail.com>
2025-04-16 15:38:46 +0200

    structs - refactor use struct Rectangle (fix test)
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,7 +14,7 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	rectangle := Rectangle{10.0, 20.0}
-	got := Area(rectanble)
+	got := Area(rectangle)
 	want := 200.0
 	if got != want {
 		t.Errorf("got %g want %g", got, want)
