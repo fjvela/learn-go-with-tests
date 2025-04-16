348abd05d823ce79582e27e8e977b369f1211c11
Javier Vela <fjvela@gmail.com>
2025-04-16 15:45:12 +0200

    structs - area update test to use methods of struct
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -16,7 +16,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("Area of rectangle", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 20.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 200.0
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
@@ -25,7 +25,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("Area of circle", func(t *testing.T) {
 		circle := Circle{10.0}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
