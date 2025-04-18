74820358b949f87893831ea5e75e02190e444a0b
Javier Vela <fjvela@gmail.com>
2025-04-18 15:25:43 +0200

    maps - implement Search (check key)
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- maps/maps.go
+++ maps/maps.go
@@ -1,5 +1,14 @@
 package maps
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) (string, error) {
+	result, exits := d[key]
+
+	if !exits {
+		return "", errors.New("the key does not exist")
+	}
+	return result, nil
 }
