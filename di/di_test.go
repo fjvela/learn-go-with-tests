a8914d160f15db012ae0d14011832d7ed164ba55
Javier Vela <fjvela@gmail.com>
2025-04-18 17:01:41 +0200

    di - add test Greet
    Signed-off-by: Javier Vela <fjvela@gmail.com>

---
+++ di/di_test.go
@@ -0,0 +1,18 @@
+package di
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Javi")
+
+	got := buffer.String()
+	want := "Hello, Javi"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
