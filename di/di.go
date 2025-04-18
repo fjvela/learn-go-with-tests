95bc445b0a3793aaa6127ddceae86fae02647c2e
Javier Vela <fjvela@gmail.com>
2025-04-18 17:07:49 +0200

    di - update package name & add func main
    Signed-off-by: Javier Vela <fjvela@gmail.com>

--- di/di.go
+++ di/di.go
@@ -1,10 +1,15 @@
-package di
+package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(writter *bytes.Buffer, name string) {
-	fmt.Fprintf(writter, "Hello, %s", name)
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
